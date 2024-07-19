4a1ebe0f0fa0d554f8a8cbff6b52daa6ddaacca9
leemhyungyu <lhglbkksj@naver.com>
2024-07-19 19:23:51 +0900

    feat: 외부 의존성 추가

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
     import ProjectDescription
 
     let packageSettings = PackageSettings(
-        // Customize the product types for specific package product
-        // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,] 
-        productTypes: [:],
+        productTypes: [
+            "ComposableArchitecture": .framework,
+            "Kingfisher": .framework,
+            "Alamofire": .framework,
+            "Moya": .framework,
+        ],
         baseSettings: .settings(
             configurations: [
                 .debug(name: "debug"),
@@ -21,8 +23,9 @@ import PackageDescription
 let package = Package(
     name: "Bottles_iOS",
     dependencies: [
-        // Add your own dependencies here:
-        // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
-        // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.11.2"),
+        .package(url: "https://github.com/onevcat/Kingfisher", from: "7.12.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.9.1"),
+        .package(url: "https://github.com/Moya/Moya.git", exact: "15.0.3")
     ]
 )
