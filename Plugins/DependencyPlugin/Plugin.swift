34e0126aef32769a662acbe96625ab1880523dce
leemhyungyu <lhglbkksj@naver.com>
2024-07-19 17:53:44 +0900

    feat: DependendyPlugin 구현

---
+++ Plugins/DependencyPlugin/Plugin.swift
@@ -0,0 +1,10 @@
+//
+//  Plugin.swift
+//  DependencyPlugin
+//
+//  Created by 임현규 on 7/17/24.
+//
+
+import ProjectDescription
+
+let plugin = Plugin(name: "DependencyPlugin")
---
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/Modules.swift
@@ -0,0 +1,70 @@
+//
+//  Modules.swift
+//  DependencyPlugin
+//
+//  Created by 임현규 on 7/17/24.
+//
+
+import ProjectDescription
+
+public enum ModulePath {
+    case feature(Feature)
+    case domain(Domain)
+    case core(Core)
+    case shared(Shared)
+}
+
+// MARK: - AppModule
+
+public extension ModulePath {
+    enum App: String, CaseIterable {
+        case iOS
+        public static let name: String = "App"
+    }
+}
+
+
+// MARK: - FeatureModule
+public extension ModulePath {
+    enum Feature: String, CaseIterable {
+        case Home
+        case Login
+
+        public static let name: String = "Feature"
+    }
+}
+
+// MARK: - DomainModule
+
+public extension ModulePath {
+    enum Domain: String, CaseIterable {
+        case Auth
+
+        public static let name: String = "Domain"
+    }
+}
+
+// MARK: - CoreModule
+
+public extension ModulePath {
+    enum Core: String, CaseIterable {
+        case Logger
+        case Network
+
+        public static let name: String = "Core"
+    }
+}
+
+// MARK: - SharedModule
+
+public extension ModulePath {
+    enum Shared: String, CaseIterable {
+        case ThirdPartyLib
+        case DesignSystem
+        case Util
+//        case ThirdPartyLib
+//        case Util
+        
+        public static let name: String = "Shared"
+    }
+}
