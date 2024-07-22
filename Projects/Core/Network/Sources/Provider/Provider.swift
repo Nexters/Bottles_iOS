b3824fe7358e23b55950aed6206ae716c8f9574d
leemhyungyu <lhglbkksj@naver.com>
2024-07-22 16:01:17 +0900

    feat: Provider 구현
    - Moya Provider을 aync/await로 래핑하는 객체

---
+++ Projects/Core/Network/Sources/Provider/Provider.swift
@@ -0,0 +1,32 @@
+//
+//  Provider.swift
+//  CoreNetworkInterface
+//
+//  Created by 임현규 on 7/22/24.
+//
+
+import Foundation
+import CoreNetworkInterface
+import Moya
+
+struct Provider<APIType: BaseTargetType>: Providable {
+  private let moyaProvider = MoyaProvider<APIType>()
+  
+  func reqeust(api: APIType) async throws -> Response {
+    return try await withCheckedThrowingContinuation { continuation in
+      moyaProvider.request(api) { result in
+        switch result {
+        case let .success(response) where 200..<300 ~= response.statusCode:
+          continuation.resume(returning: response)
+        case let .success(response) where 300... ~= response.statusCode:
+          continuation.resume(throwing: MoyaError.statusCode(response))
+        case let .failure(error):
+          continuation.resume(throwing: error)
+        default:
+          let error = NSError(domain: "Unkowned Error", code: 0)
+          continuation.resume(throwing: error)
+        }
+      }
+    }
+  }
+}
