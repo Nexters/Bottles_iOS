ca702b10501a323fd2872abd0b9b1e47f7f4ad9b
leemhyungyu <lhglbkksj@naver.com>
2024-07-28 23:32:30 +0900

    feat: Popup 하단 삼각형 뷰 구현

---
+++ Projects/Shared/DesignSystem/Sources/Components/Popup/TriangleView.swift
@@ -0,0 +1,28 @@
+//
+//  TriangleView.swift
+//  SharedDesignSystem
+//
+//  Created by 임현규 on 7/28/24.
+//
+
+import SwiftUI
+
+struct TriangleView: View {
+  private let triangleWidth: CGFloat = 10.39
+  private let triangleHeight: CGFloat = 6
+  
+  var body: some View {
+    GeometryReader { geometry in
+      let xPos = geometry.size.width / 2 - triangleWidth / 2, yPos = geometry.size.height
+      Path { path in
+        path.move(to: CGPoint(x: xPos, y: yPos))
+        path.addLine(to: CGPoint(x: xPos + triangleWidth, y: yPos))
+        path.addLine(to: CGPoint(x: xPos + triangleWidth / 2, y: yPos + triangleHeight))
+        path.addLine(to: CGPoint(x: xPos, y: yPos))
+      }
+      .stroke(ColorToken.border(.primary).color,
+              style: StrokeStyle(lineCap: .round, lineJoin: .round))
+      .fill(ColorToken.border(.primary).color)
+    }
+  }
+}
