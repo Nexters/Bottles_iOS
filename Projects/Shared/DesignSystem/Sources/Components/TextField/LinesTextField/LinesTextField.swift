ed306ca37f3c110da9bced283b8dfdca0d533f65
leemhyungyu <lhglbkksj@naver.com>
2024-07-28 22:17:44 +0900

    feat: textFieldState에 따른 색상 설정

--- Projects/Shared/DesignSystem/Sources/Components/TextField/LinesTextField/LinesTextField.swift
+++ Projects/Shared/DesignSystem/Sources/Components/TextField/LinesTextField/LinesTextField.swift
@@ -6,30 +6,44 @@
 //
 
 import SwiftUI
+import Combine
 
 public struct LinesTextField: View {
-  @State private var content: String = ""
-
+  @Binding private var text: String
+  @Binding private var textFieldState: TextFieldState
+  
   private let textFieldType: LinesTextFieldType
   private let errorMessage: String?
   private let buttonTitle: String?
   private let placeHolder: String
+  private let textLimit: Int
   
-  public init(textFieldType: LinesTextFieldType, placeHolder: String, buttonTitle: String? = nil, errorMessage: String? = nil) {
+  public init(
+    textFieldType: LinesTextFieldType,
+    textFieldState: Binding<TextFieldState>,
+    text: Binding<String>,
+    placeHolder: String,
+    buttonTitle: String? = nil,
+    errorMessage: String? = nil,
+    textLimit: Int
+  ) {
     self.textFieldType = textFieldType
+    self._textFieldState = textFieldState
+    self._text = text
     self.placeHolder = placeHolder
     self.buttonTitle = buttonTitle
     self.errorMessage = errorMessage
+    self.textLimit = textLimit
   }
   
   public var body: some View {
     RoundedRectangle(cornerRadius: BottleRadiusType.xl.value)
       .strokeBorder(
-        ColorToken.border(.enabled).color,
+        borderColor.color,
         lineWidth: 1
       )
       .frame(height: height)
-      .background(to: .container(.enablePrimary))
+      .background(to: containerColor)
       .overlay(alignment: .center) {
         linesTextFieldForm
           .padding(.md)
@@ -46,46 +60,34 @@ public extension LinesTextField {
   }
 }
 
-// MARK: - Private Extension
-
-private extension LinesTextField {
-  var height: CGFloat {
-    switch textFieldType {
-    case .introduction: return 197
-    case .letter:       return 186
-    }
-  }
-  
-  var textCountLimit: Int {
-    switch textFieldType {
-    case .introduction: return 100
-    case .letter:       return 150
-    }
-  }
-}
-
 // MARK: - Views
 
 private extension LinesTextField {
   var textCounter: some View {
-    Text("\(content.count) / \(textCountLimit)")
+    Text("\(text.count) / \(textLimit)")
       .font(to: .wantedSans(.body))
-      .foregroundStyle(to: .text(.enableTertiary))
+      .foregroundStyle(to: textColor)
       .padding(.md)
-      .onChange(of: content) { _, newValue in
-        if newValue.count > textCountLimit {
-          content = String(newValue.prefix(textCountLimit))
-        }
-      }
+  }
+  
+  var placeHolderText: some View {
+    Text(placeHolder)
+        .padding(.leading, 5)
+        .padding(.top, 8)
+        .font(to: .wantedSans(.body))
+        .foregroundStyle(to: textColor)
   }
   
   @ViewBuilder
-  var textEditer: some View {
+  var textEditor: some View {
     GeometryReader { geometry in
       let height = geometry.size.height
-      TextEditor(text: $content)
+      TextEditor(text: $text)
+        .background(alignment: .topLeading) {
+          if text.isEmpty && textFieldState == .enabled { placeHolderText }
+        }
         .padding(.md)
-        .background(to: .onContainer(.enablePrimary))
+        .background(to: textEditorColor)
         .clipShape(RoundedRectangle(cornerRadius: BottleRadiusType.sm.value))
         .scrollContentBackground(.hidden)
         .font(to: .wantedSans(.body))
@@ -100,10 +102,10 @@ private extension LinesTextField {
   var linesTextFieldForm: some View {
     switch textFieldType {
     case .introduction:
-      textEditer
+      textEditor
     case .letter:
       VStack(spacing: .md) {
-        textEditer
+        textEditor
         SolidButton(
           title: buttonTitle ?? "",
           sizeType: .medium,
@@ -111,6 +113,53 @@ private extension LinesTextField {
           action: {}
         )
       }
+    }
+  }
+}
+
+// MARK: - Private Extension
+
+private extension LinesTextField {
+  var height: CGFloat {
+    switch textFieldType {
+    case .introduction: return 197
+    case .letter:       return 186
+    }
+  }
+  
+  var textColor: ColorToken {
+    switch textFieldState {
+    case .enabled: return .text(.enableTertiary)
+    case .active:  return .text(.activePrimary)
+    case .focused: return .text(.focusePrimary)
+    case .error:   return .text(.errorSecondary)
+    }
+  }
+  
+  var borderColor: ColorToken {
+    switch textFieldState {
+    case .enabled: return .border(.enabled)
+    case .active:  return .border(.active)
+    case .focused: return .border(.focused)
+    case .error:   return .border(.error)
+    }
+  }
+  
+  var textEditorColor: ColorToken {
+    switch textFieldState {
+    case .enabled: return .onContainer(.enablePrimary)
+    case .active:  return .onContainer(.active)
+    case .focused: return .onContainer(.focuse)
+    case .error:   return .onContainer(.error)
+    }
+  }
+  
+  var containerColor: ColorToken {
+    switch textFieldState {
+    case .enabled: return .container(.enablePrimary)
+    case .active:  return .container(.Active)
+    case .focused: return .container(.focuseSecondary)
+    case .error:   return .container(.errorSecondary)
     }
   }
 }
