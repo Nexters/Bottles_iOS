388e38abb31c3d0e4f14a2ee2a33a4860bda160a
leemhyungyu <lhglbkksj@naver.com>
2024-07-29 22:57:22 +0900

    feat: BottleListResponseDTO 구현

---
+++ Projects/Domain/Bottle/Interface/Sources/DTO/Response/BottleListResponseDTO.swift
@@ -0,0 +1,23 @@
+//
+//  BottleListResponseDTO.swift
+//  DomainBottle
+//
+//  Created by 임현규 on 7/29/24.
+//
+
+import Foundation
+
+// MARK: - BottleList
+public struct BottleListResponseDTO: Decodable {
+  public let bottles: [BottleItemDTO]
+  
+  // MARK: - BottleItem
+  public struct BottleItemDTO: Codable {
+    let age: Int
+    let expiredAt: String
+    let id: Int
+    let keyword: [String]
+    let mbti, userName: String
+  }
+}
+
