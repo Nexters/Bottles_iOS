c6a1d5cdf835d74e05b6a6679c7f9e96092deb57
leemhyungyu <lhglbkksj@naver.com>
2024-07-29 21:25:14 +0900

    feat: ProfileResponseDTO 구현

---
+++ Projects/Domain/Profile/Interface/Sources/DTO/Response/ProfileResponseDTO.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileResponseDTO.swift
+//  DomainProfileInterface
+//
+//  Created by 임현규 on 7/29/24.
+//
+
+import Foundation
+
+// MARK: - Profile
+public struct ProfileResponseDTO: Decodable {
+  public let userName: String
+  public let age: Int
+  public let introduction: [IntroductionDTO]?
+  public let profileSelect: ProfileSelectDTO?
+  
+  // MARK: - Introduction
+  public struct IntroductionDTO: Decodable {
+    let answer, question: String
+  }
+
+  // MARK: - ProfileSelect
+  public struct ProfileSelectDTO: Decodable {
+    let alcohol: String
+    let height: Int
+    let interest: InterestDTO
+    let job: String
+    let keyword: [String]
+    let mbti: String
+    let region: RegionDTO
+    let religion, smoking: String
+  }
+
+  // MARK: - Interest
+  struct InterestDTO: Decodable {
+    let culture, entertainment, etc, sports: [String]
+  }
+
+  // MARK: - Region
+  struct RegionDTO: Decodable {
+    let city, state: String
+  }
+}
+
