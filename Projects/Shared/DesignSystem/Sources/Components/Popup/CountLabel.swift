154a0f73399832a73664c61d7bafcab77748a6a8
leemhyungyu <lhglbkksj@naver.com>
2024-07-30 15:03:04 +0900

    feat: CountLabel 구현

---
+++ Projects/Shared/DesignSystem/Sources/Components/Popup/CountLabel.swift
@@ -0,0 +1,31 @@
+//
+//  CountLabel.swift
+//  SharedDesignSystem
+//
+//  Created by 임현규 on 7/30/24.
+//
+
+import SwiftUI
+
+struct CountLabel: View {
+  private let radius: CGFloat = 24
+  private let text: String
+  
+  init(text: String) {
+    self.text = text
+  }
+  
+  var body: some View {
+    WantedSansStyleText(
+      text,
+      style: .body,
+      color: .quaternary
+    )
+    .padding(4)
+    .frame(minWidth: radius)
+    .frame(height: radius)
+    .frame(maxWidth: nil)
+    .background(to: .icon(.update))
+    .clipShape(RoundedRectangle(cornerRadius: radius / 2))
+  }
+}
