6a96270a8695c5b2d6a2341996d19f125fc0d1e8
leemhyungyu <lhglbkksj@naver.com>
2024-07-30 23:17:54 +0900

    refactor: DTO 옵셔널 처리로 수정

--- Projects/Domain/Bottle/Interface/Sources/DTO/Response/BottleListResponseDTO.swift
+++ Projects/Domain/Bottle/Interface/Sources/DTO/Response/BottleListResponseDTO.swift
@@ -9,16 +9,16 @@ import Foundation
 
 // MARK: - BottleList
 public struct BottleListResponseDTO: Decodable {
-  public let bottles: [BottleItemDTO]
+  public let bottles: [BottleItemDTO]?
   
   // MARK: - BottleItem
   public struct BottleItemDTO: Codable {
-    let age: Int
-    let expiredAt: String
-    let id: Int
+    let age: Int?
+    let expiredAt: String?
+    let id: Int?
     let keyword: [String]?
     let mbti: String?
-    let userName: String
+    let userName: String?
   }
 }
 
