2c5bb3c936f165820bb758f93787d39b2758eec9
leemhyungyu <lhglbkksj@naver.com>
2024-08-02 19:08:00 +0900

    feat: 자기소개 작성 여부  ResponseDTO 구현

---
+++ Projects/Domain/Profile/Interface/Sources/DTO/Response/IntroductionExistResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  IntroductionExistResponseDTO.swift
+//  DomainProfile
+//
+//  Created by 임현규 on 8/2/24.
+//
+
+import Foundation
+
+public struct IntroductionExistResponseDTO: Decodable {
+  public let isExist: Bool?
+}
