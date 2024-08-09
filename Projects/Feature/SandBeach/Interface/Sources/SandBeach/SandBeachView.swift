d07d291c937f3dc872d223758ad852c08d11f2c8
leemhyungyu <lhglbkksj@naver.com>
2024-08-10 00:44:03 +0900

    feat: WithPerceptionTracking 추가

--- Projects/Feature/SandBeach/Interface/Sources/SandBeach/SandBeachView.swift
+++ Projects/Feature/SandBeach/Interface/Sources/SandBeach/SandBeachView.swift
@@ -21,44 +21,46 @@ public struct SandBeachView: View {
   public var body: some View {
     WithPerceptionTracking {
       GeometryReader { geo in
-        if store.isLoading {
-          LoadingIndicator()
-        } else {
-          VStack(spacing: 0) {
-            Spacer()
-            BottleImageView(type: .local(bottleImageSystem: .illustraition(.logo)))
-              .frame(width: 78.06, height: 20)
-              .padding(.top, geo.safeAreaInsets.top + 14)
-              .padding(.bottom, 38)
-            
-            WantedSansStyleText(
-              store.userState.title, style: .title1, color: .secondary)
-            .frame(height: 62)
-            .multilineTextAlignment(.center)
-            .padding(.bottom, 24)
-            Spacer()
-            
-            popup
-              .padding(.bottom, 8)
-            
-            BottleImageView(type: .local(
-              bottleImageSystem:
-                store.userState.isEmptyBottle ? .illustraition(.islandEmptyBottle) : .illustraition(.islandHasBottle))
-            )
-            .frame(width: geo.size.width)
-            .frame(height: geo.size.width)
-            .asThrottleButton {
-              if store.userState.isHasNewBottle {
-                store.send(.newBottleIslandDidTapped)
-              }
+        WithPerceptionTracking {
+          if store.isLoading {
+            LoadingIndicator()
+          } else {
+            VStack(spacing: 0) {
+              Spacer()
+              BottleImageView(type: .local(bottleImageSystem: .illustraition(.logo)))
+                .frame(width: 78.06, height: 20)
+                .padding(.top, geo.safeAreaInsets.top + 14)
+                .padding(.bottom, 38)
               
-              if store.userState.isHasActiveBottle {
-                store.send(.bottleStorageIslandDidTapped)
+              WantedSansStyleText(
+                store.userState.title, style: .title1, color: .secondary)
+              .frame(height: 62)
+              .multilineTextAlignment(.center)
+              .padding(.bottom, 24)
+              Spacer()
+              
+              popup
+                .padding(.bottom, 8)
+              
+              BottleImageView(type: .local(
+                bottleImageSystem:
+                  store.userState.isEmptyBottle ? .illustraition(.islandEmptyBottle) : .illustraition(.islandHasBottle))
+              )
+              .frame(width: geo.size.width)
+              .frame(height: geo.size.width)
+              .asThrottleButton {
+                if store.userState.isHasNewBottle {
+                  store.send(.newBottleIslandDidTapped)
+                }
+                
+                if store.userState.isHasActiveBottle {
+                  store.send(.bottleStorageIslandDidTapped)
+                }
               }
+              .disabled(store.isDisableIslandBottle)
+              
+              Spacer()
             }
-            .disabled(store.isDisableIslandBottle)
-            
-            Spacer()
           }
         }
       }
