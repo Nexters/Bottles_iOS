2479f41bd5edacc4421c596684050681c933711d
leemhyungyu <lhglbkksj@naver.com>
2024-08-11 02:28:32 +0900

    feat: 도착한 보틀 View 구현

---
+++ Projects/Feature/BottleArrival/Interface/Sources/BottleArrivalView.swift
@@ -0,0 +1,49 @@
+//
+//  BottleArrivalView.swift
+//  FeatureBottleArrivalInterface
+//
+//  Created by 임현규 on 8/11/24.
+//
+
+import SwiftUI
+
+import SharedDesignSystem
+import FeatureBaseWebViewInterface
+
+import ComposableArchitecture
+
+public struct BottleArrivalView: View {
+  private let store: StoreOf<BottleArrivalFeature>
+  
+  public init(store: StoreOf<BottleArrivalFeature>) {
+    self.store = store
+  }
+  
+  public var body: some View {
+
+    WithPerceptionTracking {
+      BaseWebView(
+        type: .bottles,
+        isScrollEnabled: true) { action in
+        switch action {
+        case .webViewLoadingDidCompleted:
+          store.send(.webViewLoadingDidCompleted)
+        case .bottelDidAccepted:
+          store.send(.bottelDidAccepted)
+        case .closeWebView:
+          store.send(.closeWebView)
+        default:
+          break
+        }
+      }
+      .overlay {
+        if store.isLoading {
+          LoadingIndicator()
+        }
+      }
+      .toolbar(.hidden, for: .navigationBar)
+      .toolbar(.hidden, for: .tabBar)
+      .ignoresSafeArea(.all, edges: .bottom)
+    }
+  }
+}
