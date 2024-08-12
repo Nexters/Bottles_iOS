8bcba88a5afb69acbdcd4bd8c7a3d24db0303550
leemhyungyu <lhglbkksj@naver.com>
2024-08-12 22:59:56 +0900

    feat: MatchingView 구현

--- Projects/Feature/BottleStorage/Interface/Sources/PingPongDetail/View/SubViews/Matching/MatchingView.swift
+++ Projects/Feature/BottleStorage/Interface/Sources/PingPongDetail/View/SubViews/Matching/MatchingView.swift
@@ -6,9 +6,9 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 import SharedDesignSystem
-
 import ComposableArchitecture
 
 public struct MatchingView: View {
@@ -22,10 +22,132 @@ public struct MatchingView: View {
     WithPerceptionTracking {
       ScrollView {
         VStack(alignment: .leading, spacing: 0.0) {
-          Text("Matching")
+          title
+            .padding(.vertical, 32)
+          
+          GeometryReader { geo in
+            let width = geo.size.width - 50
+            HStack(spacing: 0 ) {
+              Spacer()
+              matchingInfo
+                .frame(width: width, height: width)
+              Spacer()
+            }
+          }
+          .aspectRatio(1, contentMode: .fit)
+          
+          Spacer()
+          bottomButton
         }
+        .padding(.horizontal, .md)
         .frame(maxHeight: .infinity)
       }
+    }
+  }
+}
+
+// MARK: - Views
+
+private extension MatchingView {
+  @ViewBuilder
+  var title: some View {
+    switch store.matchingState {
+    case .waiting:
+      TitleView(
+        title: "\(store.peerUserName ?? "")님의\n결정을 기다리고 있어요",
+        caption: "조금만 더 기다려주세요!"
+      )
+    case .success:
+      TitleView(
+        title: "축하해요! 지금부터 찐-하게\n서로를 알아가 보세요",
+        caption: "아이디를 복사해 더 깊은 대화를 나눠보세요"
+      )
+    case .failure:
+      TitleView(
+        title: "다른 보틀을\n열어보는 건 어때요",
+        caption: "아쉽지만 매칭에 실패했어요"
+      )
+    default:
+      EmptyView()
+    }
+  }
+  
+  @ViewBuilder
+  var matchingInfo: some View {
+    switch store.matchingState {
+    case .waiting:
+      BottleImageView(
+        type: .local(
+          bottleImageSystem: .illustraition(.phone)
+        )
+      )
+    case .success:
+      kakaoTalkIdShareView
+    case .failure:
+      BottleImageView(
+        type: .local(
+          bottleImageSystem: .illustraition(.bottle1)
+        )
+      )
+    default:
+      EmptyView()
+    }
+  }
+  
+  var kakaoTalkIdShareView: some View {
+    RoundedRectangle(cornerRadius: BottleRadiusType.xl.value)
+      .strokeBorder(ColorToken.border(.primary).color, lineWidth: 1)
+      .frame(maxWidth: .infinity)
+      .frame(height: 179)
+      .overlay {
+        VStack(spacing: .xl) {
+          WantedSansStyleText(
+            "카카오톡 아이디",
+            style: .body,
+            color: .quinary
+          )
+          .padding(.vertical, 2)
+          .padding(.horizontal, .xs)
+          .background {
+            RoundedRectangle(cornerRadius: BottleRadiusType.xs.value)
+              .fill(ColorToken.container(.secondary).color)
+          }
+          
+          WantedSansStyleText(
+            store.kakaoTalkId ?? "hello",
+            style: .subTitle1,
+            color: .primary
+          )
+          
+          OutlinedStyleButton(
+            .small(contentType: .image(type: .local(bottleImageSystem: .icom(.share)))),
+            title: "복사하기",
+            buttonType: .throttle
+          ) {
+            let clipboard = UIPasteboard.general
+            clipboard.setValue(store.kakaoTalkId ?? "", forPasteboardType: UTType.plainText.identifier)
+            store.send(.copyButtonDidTapped)
+          }
+        }
+      }
+  }
+  
+  @ViewBuilder
+  var bottomButton: some View {
+    switch store.matchingState {
+    case .waiting:
+      EmptyView()
+    case .success:
+      EmptyView()
+    case .failure:
+      SolidButton(
+        title: "다른 보틀 열어보기",
+        sizeType: .large,
+        buttonType: .throttle,
+        action: { store.send(.otherBottleButtonDidTapped) }
+      )
+    default:
+      EmptyView()
     }
   }
 }
