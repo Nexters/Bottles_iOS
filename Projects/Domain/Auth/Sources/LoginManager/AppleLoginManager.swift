b27093214b2c433243921cd48296840e402f4fbe
leemhyungyu <lhglbkksj@naver.com>
2024-08-20 15:58:28 +0900

    feat: AppleLoginManager 구현

---
+++ Projects/Domain/Auth/Sources/LoginManager/AppleLoginManager.swift
@@ -0,0 +1,64 @@
+//
+//  AppleLoginManager.swift
+//  DomainAuth
+//
+//  Created by 임현규 on 8/20/24.
+//
+
+import Foundation
+import AuthenticationServices
+
+final class AppleLoginManager: NSObject, ASAuthorizationControllerDelegate {
+  typealias IdentityToken = String
+  
+  private var continuation: CheckedContinuation<IdentityToken, Error>?
+  
+  func signInWithApple() async throws -> IdentityToken {
+    try await withCheckedThrowingContinuation { continuation in
+      let request = ASAuthorizationAppleIDProvider().createRequest()
+      request.requestedScopes = [.fullName, .email]
+      let controller = ASAuthorizationController(authorizationRequests: [request])
+      controller.performRequests()
+      controller.delegate = self
+      
+      if self.continuation == nil {
+        self.continuation = continuation
+      }
+    }
+  }
+  
+  func authorizationController(
+    controller: ASAuthorizationController,
+    didCompleteWithAuthorization authorization: ASAuthorization
+  ) {
+    // TODO: - Domain Error 추가.
+    
+    guard let credential = authorization.credential as? ASAuthorizationAppleIDCredential else {
+      let error = NSError(domain: "invalidCrendential", code: 0)
+      continuation?.resume(throwing: error)
+      continuation = nil
+      return
+    }
+    
+    guard let identityToken = credential.identityToken,
+          let decodedIdentityToken = String(data: identityToken, encoding: .utf8) else {
+      let error = NSError(domain: "invalidIdentityToken", code: 0)
+      continuation?.resume(throwing: error)
+      continuation = nil
+      return
+    }
+    
+    continuation?.resume(returning: decodedIdentityToken)
+    continuation = nil
+  }
+  
+  func authorizationController(
+    controller: ASAuthorizationController,
+    didCompleteWithError error: any Error
+  ) {
+    let error = NSError(domain: error.localizedDescription, code: 0)
+    continuation?.resume(throwing: error)
+    continuation = nil
+  }
+}
+
