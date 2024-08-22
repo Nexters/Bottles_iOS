6e313cac19a9d949ba476b1abb3d19d304abe354
leemhyungyu <lhglbkksj@naver.com>
2024-08-22 16:13:04 +0900

    feat: UserClient 구현

---
+++ Projects/Domain/User/Interface/Sources/UserClient.swift
@@ -0,0 +1,43 @@
+//
+//  UserClient.swift
+//  DomainUserInterface
+//
+//  Created by 임현규 on 8/22/24.
+//
+
+import Foundation
+
+public struct UserClient {
+  private let _isLoggedIn: () -> Bool
+  private let _isAppDeleted: () -> Bool
+  private let updateLoginState: (Bool) -> Void
+  private let updateDeleteState: (Bool) -> Void
+  
+  public init(
+    isLoggedIn: @escaping () -> Bool,
+    isAppDeleted: @escaping () -> Bool,
+    updateLoginState: @escaping (Bool) -> Void,
+    updateDeleteState: @escaping (Bool) -> Void
+  ) {
+    self._isLoggedIn = isLoggedIn
+    self._isAppDeleted = isAppDeleted
+    self.updateLoginState = updateLoginState
+    self.updateDeleteState = updateDeleteState
+  }
+  
+  public func fetchLoginState() -> Bool {
+    _isLoggedIn()
+  }
+  
+  public func fetchIsDeleteApp() -> Bool {
+    _isAppDeleted()
+  }
+  
+  public func updateLoginState(isLoggedIn: Bool) {
+    updateLoginState(isLoggedIn)
+  }
+  
+  public func updateDeleteState(isDelete: Bool) {
+    updateDeleteState(isDelete)
+  }
+}
