8f970fe1d48aa08309e3d6d211966812bd620005
leemhyungyu <lhglbkksj@naver.com>
2024-08-23 14:41:47 +0900

    feat: PingPongGuideView 구현

---
+++ Projects/Feature/Guide/Interface/Sources/PingPongGuide/PingPongGuideFeature.swift
@@ -0,0 +1,48 @@
+//
+//  PingPongGuideFeature.swift
+//  FeatureGuideInterface
+//
+//  Created by 임현규 on 8/22/24.
+//
+
+import Foundation
+
+import ComposableArchitecture
+
+@Reducer
+public struct PingPongGuideFeature {
+  private let reducer: Reduce<State, Action>
+  
+  init(reducer: Reduce<State, Action>) {
+    self.reducer = reducer
+  }
+  
+  public struct State: Equatable {
+    public init() {}
+  }
+  
+  public enum Action {
+    case nextButtonDidTapped
+    case backButtonDidTapped
+  }
+  
+  public var body: some ReducerOf<Self> {
+    reducer
+  }
+}
+
+extension PingPongGuideFeature {
+  public init() {
+    let reducer = Reduce<State, Action> { state, action in
+      switch action {
+      case .nextButtonDidTapped:
+        return .none
+        
+      case .backButtonDidTapped:
+        return .none
+      }
+    }
+    
+    self.init(reducer: reducer)
+  }
+}
---
+++ Projects/Feature/Guide/Interface/Sources/PingPongGuide/PingPongGuideView.swift
@@ -0,0 +1,89 @@
+//
+//  PingPongGuideView.swift
+//  FeatureGuideInterface
+//
+//  Created by 임현규 on 8/22/24.
+//
+
+import SwiftUI
+
+import SharedDesignSystem
+
+import ComposableArchitecture
+
+public struct PingPongGuideView: View {
+  private let store: StoreOf<PingPongGuideFeature>
+  
+  public init(store: StoreOf<PingPongGuideFeature>) {
+    self.store = store
+  }
+  
+  public var body: some View {
+    VStack(spacing: 0) {
+      title
+      ZStack(alignment: .bottom) {
+        GeometryReader { geometry in
+          
+          HStack(spacing: 0) {
+            Spacer()
+            VStack(spacing: 10) {
+              firstPingPongImage
+                .aspectRatio(contentMode: .fit)
+                .frame(width: geometry.size.width - 70)
+                .clipped()
+              secondPingPongImage
+                .aspectRatio(contentMode: .fit)
+                .frame(width: geometry.size.width - 55)
+                .clipped()
+            }
+            Spacer()
+          }
+          .offset(y: 48)
+        }
+        nextButton
+          .padding(.bottom, .lg)
+          .background(to: ColorToken.container(.primary))
+      }
+      .border(.red)
+    }
+    .padding(.top, .xl)
+    .padding(.horizontal, .md)
+    .setNavigationBar {
+      makeNaivgationleftButton {
+        store.send(.backButtonDidTapped)
+      }
+    }
+  }
+}
+
+private extension PingPongGuideView {
+  var title: some View {
+    TitleView(
+      pageInfo: PageInfo(nowPage: 2, totalCount: 4),
+      title: "보틀만의 가지관 문답을 통해\n진솔한 모습을 확인할 수 있어요"
+    )
+  }
+  
+  var firstPingPongImage: some View {
+    BottleImageView(type:
+        .local(bottleImageSystem: .illustraition(.firstPingPong))
+    )
+  }
+  
+  var secondPingPongImage: some View {
+    BottleImageView(type:
+        .local(bottleImageSystem: .illustraition(.secondPingPong))
+    )
+  }
+  
+  var nextButton: some View {
+      SolidButton(
+        title: "다음",
+        sizeType: .large,
+        buttonType: .throttle,
+        action: { store.send(.nextButtonDidTapped) }
+      )
+      .shadow(color: .white, radius: 15, y: -30)
+    
+  }
+}
