343c957ba6d406f56534ad5c44b454c7f96f5925
leemhyungyu <lhglbkksj@naver.com>
2024-08-24 02:32:16 +0900

    feat: FCM Token UserClient로 관리

--- Projects/Domain/User/Interface/Sources/UserClient.swift
+++ Projects/Domain/User/Interface/Sources/UserClient.swift
@@ -10,19 +10,25 @@ import Foundation
 public struct UserClient {
   private let _isLoggedIn: () -> Bool
   private let _isAppDeleted: () -> Bool
+  private let _fetchFcmToken: () -> String?
   private let updateLoginState: (Bool) -> Void
   private let updateDeleteState: (Bool) -> Void
+  private let updateFcmToken: (String) -> Void
   
   public init(
     isLoggedIn: @escaping () -> Bool,
     isAppDeleted: @escaping () -> Bool,
+    fetchFcmToken: @escaping () -> String?,
     updateLoginState: @escaping (Bool) -> Void,
-    updateDeleteState: @escaping (Bool) -> Void
+    updateDeleteState: @escaping (Bool) -> Void,
+    updateFcmToken: @escaping (String) -> Void
   ) {
     self._isLoggedIn = isLoggedIn
     self._isAppDeleted = isAppDeleted
+    self._fetchFcmToken = fetchFcmToken
     self.updateLoginState = updateLoginState
     self.updateDeleteState = updateDeleteState
+    self.updateFcmToken = updateFcmToken
   }
   
   public func isLoggedIn() -> Bool {
@@ -33,11 +39,19 @@ public struct UserClient {
     _isAppDeleted()
   }
   
+  public func fetchFcmToken() -> String? {
+    _fetchFcmToken()
+  }
+  
   public func updateLoginState(isLoggedIn: Bool) {
     updateLoginState(isLoggedIn)
   }
   
   public func updateDeleteState(isDelete: Bool) {
     updateDeleteState(isDelete)
+  }
+  
+  public func updateFcmToken(fcmToken: String) {
+    updateFcmToken(fcmToken)
   }
 }
