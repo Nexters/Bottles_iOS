1356b25c7c4762281820488e08684f24786f0106
leemhyungyu <lhglbkksj@naver.com>
2024-08-23 14:41:33 +0900

    feat: MianGuideView bottleArrivalImage 비율 수정

--- Projects/Feature/Guide/Interface/Sources/MainGuide/MainGuideView.swift
+++ Projects/Feature/Guide/Interface/Sources/MainGuide/MainGuideView.swift
@@ -19,10 +19,25 @@ public struct MainGuideView: View {
   }
   
   public var body: some View {
+    
     VStack(spacing: 0) {
       title
-      bottleArrivalImage
-      nextButton
+      ZStack(alignment: .bottom) {
+        GeometryReader { geometry in
+          HStack(spacing: 0) {
+            Spacer()
+            bottleArrivalImage
+              .aspectRatio(contentMode: .fill)
+              .frame(width: geometry.size.width - 72)
+              .clipped()
+              .offset(y: 48)
+            Spacer()
+          }          
+        }
+        nextButton
+          .padding(.bottom, .lg)
+          .background(to: ColorToken.container(.primary))
+      }
     }
     .padding(.top, .xl)
     .padding(.horizontal, .md)
@@ -37,8 +52,8 @@ public struct MainGuideView: View {
 private extension MainGuideView {
   var title: some View {
     TitleView(
-      pageInfo: PageInfo(nowPage: 1, totalCount: 5),
-      title: "천천히 서로를 알아가는 소개팅\n보틀에 오신 것을 환영해요"
+      pageInfo: PageInfo(nowPage: 1, totalCount: 4),
+      title: "천천히 서로를 알아가는 소개팅\n보틀에 오신 것을 환영해요!"
     )
   }
   
@@ -46,18 +61,23 @@ private extension MainGuideView {
     BottleImageView(type:
         .local(bottleImageSystem: .illustraition(.bottleArrivalPhone))
     )
-    .clipped()
-    .padding(.top, 48)
+    .clipShape(RoundedRectangle(cornerRadius: 30))
+    .overlay(
+      RoundedRectangle(cornerRadius: 30)
+        .strokeBorder(
+          ColorToken.text(.primary).color,
+          lineWidth: 10
+        )
+    )
   }
   
   var nextButton: some View {
-      SolidButton(
-        title: "다음",
-        sizeType: .large,
-        buttonType: .throttle,
-        action: { store.send(.nextButtonDidTapped) }
-      )
-      .shadow(color: .white, radius: 30, y: -30)
-    
+    SolidButton(
+      title: "다음",
+      sizeType: .large,
+      buttonType: .throttle,
+      action: { store.send(.nextButtonDidTapped) }
+    )
+    .shadow(color: .white, radius: 15, y: -30)
   }
 }
