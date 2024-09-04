04086e06c4729e73a9d96e9ad5b09c622314aed3
leemhyungyu <lhglbkksj@naver.com>
2024-09-05 00:09:35 +0900

    chore: 빌드 넘버 1.0.6 (26)

--- Tuist/ProjectDescriptionHelpers/InfoPlist+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/InfoPlist+Templates.swift
@@ -10,8 +10,8 @@ import ProjectDescription
 public extension InfoPlist {
     static var app: InfoPlist {
         return .extendingDefault(with: [
-            "CFBundleShortVersionString": "1.0.5",
-            "CFBundleVersion": "25",
+            "CFBundleShortVersionString": "1.0.6",
+            "CFBundleVersion": "26",
             "UIUserInterfaceStyle": "Light",
             "CFBundleName": "보틀",
             "UILaunchScreen": [
@@ -39,8 +39,8 @@ public extension InfoPlist {
     
     static var example: InfoPlist {
         return .extendingDefault(with: [
-            "CFBundleShortVersionString": "1.0.5",
-            "CFBundleVersion": "25",
+            "CFBundleShortVersionString": "1.0.6",
+            "CFBundleVersion": "26",
             "UIUserInterfaceStyle": "Light",
             "UILaunchScreen": [:],
             "UISupportedInterfaceOrientations": [
