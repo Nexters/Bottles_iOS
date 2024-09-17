b86b435ddb4d8037f95eb171baad9688c41881e6
leemhyungyu <lhglbkksj@naver.com>
2024-09-17 14:53:19 +0900

    feat: 화살표 클릭시 action 실행하도록 수정

--- Projects/Shared/DesignSystem/Sources/Components/List/ArrowListView.swift
+++ Projects/Shared/DesignSystem/Sources/Components/List/ArrowListView.swift
@@ -8,15 +8,18 @@
 import SwiftUI
 
 public struct ArrowListView: View {
-  public let title: String
-  public let subTitle: String?
+  private let title: String
+  private let subTitle: String?
+  private let action: () -> Void
   
   public init(
     title: String,
-    subTitle: String? = nil
+    subTitle: String? = nil,
+    action: @escaping () -> Void
   ) {
     self.title = title
     self.subTitle = subTitle
+    self.action = action
   }
   
   public var body: some View {
@@ -39,5 +42,6 @@ private extension ArrowListView {
     .foregroundStyle(to: ColorToken.icon(.primary))
     .frame(width: 24)
     .frame(height: 24)
+    .asThrottleButton(action: action)
   }
 }
