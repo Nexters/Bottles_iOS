3e0aa2e7e1e10b5550fda74c7a17ab93d6998d52
leemhyungyu <lhglbkksj@naver.com>
2024-10-28 23:16:30 +0900

    feat: SandBeachCoachMarkFeature 구현

---
+++ Projects/Feature/SandBeach/Interface/Sources/SandBeachCoachMark/SandBeachCoachMarkFeature.swift
@@ -0,0 +1,22 @@
+//
+//  SandBeachCoachMarkFeature.swift
+//  FeatureSandBeachInterface
+//
+//  Created by 임현규 on 10/28/24.
+//
+
+import ComposableArchitecture
+
+extension SandBeachCoachMarkFeature {
+  public init() {
+    let reducer = Reduce<State, Action> { state, action in
+      switch action {
+      case .coachMarkDidTapped:
+        state.count += 1
+        return .none
+      }
+    }
+    
+    self.init(reducer: reducer)
+  }
+}
