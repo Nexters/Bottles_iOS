24b77715b3ad53efa2f5dd63239a43882615ff37
JongHoon <qnm541@icloud.com>
2024-11-10 00:43:48 +0900

    chore: profile setup interface에 base webview interface 의존성 주입

--- Projects/Feature/ProfileSetup/Project.swift
+++ Projects/Feature/ProfileSetup/Project.swift
@@ -9,7 +9,8 @@ let project = Project.makeModule(
             interface: .ProfileSetup,
             factory: .init(
                 dependencies: [
-                    .domain
+                    .domain,
+                    .feature(interface: .BaseWebView)
                 ]
             )
         ),
