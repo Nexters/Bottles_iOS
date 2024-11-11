47c5d5158b355264098e66b4e48abbe611511ae8
JongHoon <qnm541@icloud.com>
2024-11-12 00:25:07 +0900

    chore: 빌드넘버 업데이트 (39)

--- Tuist/ProjectDescriptionHelpers/InfoPlist+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/InfoPlist+Templates.swift
@@ -11,7 +11,7 @@ public extension InfoPlist {
     static var app: InfoPlist {
         return .extendingDefault(with: [
             "CFBundleShortVersionString": "1.0.11",
-            "CFBundleVersion": "38",
+            "CFBundleVersion": "39",
             "UIUserInterfaceStyle": "Light",
             "CFBundleName": "보틀",
             "UILaunchScreen": [
@@ -46,7 +46,7 @@ public extension InfoPlist {
     static var example: InfoPlist {
         return .extendingDefault(with: [
             "CFBundleShortVersionString": "1.0.11",
-            "CFBundleVersion": "38",
+            "CFBundleVersion": "39",
             "UIUserInterfaceStyle": "Light",
             "UILaunchScreen": [:],
             "UISupportedInterfaceOrientations": [
